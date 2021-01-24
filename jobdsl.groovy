418659054d8b1df4a70c88a5da68a9970f26c4ee
Ireneusz Pastusiak <ireneusz.pastusiak@gmail.com>
2021-01-24 12:07:36 +0100

    Add git id
    https://issues.jenkins.io/browse/JENKINS-43693

--- jobdsl.groovy
+++ jobdsl.groovy
@@ -3,6 +3,7 @@
 multibranchPipelineJob('enforcer-rules') {
     branchSources {
         git {
+            id('160F213D-6B5A-4997-A88C-8CEB1A7C4E60')
             remote('git@github.com:automatictester/enforcer-rules.git')
             credentialsId('github-creds')
         }
